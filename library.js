180df718d3421fecc10c954c4c0270fb8a5e1496
Noah Kremer <75767608+nkremer20@users.noreply.github.com>
2025-07-24 00:27:00 -0500

    Creating function to create new cards from the library array

--- library.js
+++ library.js
@@ -11,8 +11,41 @@ function Book(title, author, pages, read) {
   this.id = crypto.randomUUID();
   };
 
-function addBookToLibrary(new_book) {
+function addBookToLibrary() {
+    // Get the last item in the array
+    const book = library[library.length - 1];
+
+    // Create new card in the library div
+    const library_div = document.querySelector('.library');
+    const new_card = document.createElement('div');
+    new_card.classList.add('book-card');
     
+
+    // Add object info to new card
+    const new_card_info = document.createElement('div');
+
+    for (const key of Object.keys(book)) {
+        const card_info = document.createElement('h4')
+        card_info.classList.add('card-text');
+        if (key != 'id') {
+            card_info.textContent = key.charAt(0).toUpperCase() + key.slice(1);
+            
+            if (key == 'read') {
+
+            }
+            else {
+
+            }
+        }
+        else {
+            new_card.id = book[key];
+        }
+        new_card_info.appendChild(card_info);
+    }
+
+    new_card.appendChild(new_card_info);
+    library_div.appendChild(new_card);
+        
 };
 
 window.onload = () => {
@@ -42,7 +75,10 @@ window.onload = () => {
         // Create new book object
         const new_book = new Book(title, author, pages, read);
 
-        console.log(new_book);
+        // Add book to library array
+        library.push(new_book);
+
+        addBookToLibrary();
 
         form.reset();
     })
